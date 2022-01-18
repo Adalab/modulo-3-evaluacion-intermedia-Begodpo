b7596e258d9bf2f814e675260986d7467a03aa39
Begodpo <begodpo@gmail.com>
2022-01-18 10:13:00 +0100

    render adalabers list

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,35 @@
 import "../styles/App.scss";
+import adalaberList from "../data/contacts.json";
 
 function App() {
+  const adalabers = adalaberList.results;
+  console.log(adalabers);
+  const renderAdalabers = () => {
+    return adalabers.map((adalaber) => {
+      console.log(adalaber);
+      return (
+        <tbody key={adalaber.id}>
+          <tr>
+            <td>{adalaber.name}</td>
+            <td>{adalaber.counselor}</td>
+            <td>{adalaber.speciality}</td>
+          </tr>
+        </tbody>
+      );
+    });
+  };
+
   return (
-    <div>
-      <h1>Hola mundo</h1>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Tutora</th>
+          <th>Especialidad</th>
+        </tr>
+      </thead>
+      {renderAdalabers()}
+    </table>
   );
 }
 
